2bfd1e878b12cd1e4d027fd9dc4d6ef6d29c44b7
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-09-28 22:57:12 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
     <main>
       <div className="container">
         <div className="row">
-          <div className="col-3">
+          <div className="col-12 col-lg-3 mb-4">
             <div className="bg-primary px-3 py-2 rounded">TODO</div>
             <div>
               {Object.keys(tasksList["todo"]).map((key) => {
@@ -66,7 +66,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="col-3">
+          <div className="col-12 col-lg-3 mb-4">
             <div className="bg-warning px-3 py-2 rounded">In progress</div>
             <div>
               {Object.keys(tasksList["in_progress"]).map((key) => {
@@ -79,7 +79,7 @@ export default function Home() {
               })}
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-lg-3 mb-4">
             <div className="bg-success px-3 py-2 rounded">Done</div>
             <div>
               {Object.keys(tasksList["done"]).map((key) => {
@@ -92,7 +92,7 @@ export default function Home() {
               })}
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-lg-3">
             <Link href={"/tasks/create"} className="btn btn-dark ">
               Add new task +
             </Link>
