07758a909030d0e0542452c44adad6355f973048
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-09-30 01:25:58 +0530

    Update TaskCard.jsx

--- app/componets/TaskCard.jsx
+++ app/componets/TaskCard.jsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
 export default function TaskCard({ props }) {
   return (
@@ -10,12 +8,12 @@ export default function TaskCard({ props }) {
           <div className="card-body">
             <div className="d-flex justify-content-between">
               <h5>{props.title} </h5>
-              {/* <span>
-                <FontAwesomeIcon
-                  icon={faPenToSquare}
+              <span>
+                <i
+                  class="fa-regular fa-pen-to-square"
                   style={{ color: "#7b8493" }}
-                />
-              </span> */}
+                ></i>
+              </span>
             </div>
           </div>
         </div>
