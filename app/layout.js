678be1a0bfb4aaa8a35dabd2f2cec7afc87a37ee
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-09-30 01:26:31 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,10 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header></Header>
         <div className="container mt-3">{children}</div>
+        <script
+          src="https://kit.fontawesome.com/3f8256dd02.js"
+          crossorigin="anonymous"
+        ></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
       </body>
